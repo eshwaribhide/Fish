812866e2e38a59ee5561f36ff3e356af02d7bd5e
Eshwari Bhide <52673645+eshwaribhide@users.noreply.github.com>
2020-12-13 16:44:09 -0800

    Update tile_fish_penguin_constants.py

--- Common/tile_fish_penguin_constants.py
+++ Common/tile_fish_penguin_constants.py
@@ -1,11 +1,5 @@
-"""
-This is explained in our README, too. But we noticed that certain aspects seemed to be uniform
-and reusable across different files. Therefore, we placed them in this constants file in order
-to not use any magic numbers.
-"""
 TILE_SIZE = 40
 
-# Our tiles accommodate up to 7 fish per tile if the tile size is <=40, which is what we deem an acceptable tile size
 MAX_FISH = 7
 
 FISH_SIZE = 2*(TILE_SIZE-10) / MAX_FISH
