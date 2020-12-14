1c9ce64ec6a7d92626956d8400a2a025065b6c5b
Eshwari Bhide <52673645+eshwaribhide@users.noreply.github.com>
2020-12-13 16:42:32 -0800

    Update penguin.py

--- Common/penguin.py
+++ Common/penguin.py
@@ -3,11 +3,10 @@ from tile_fish_penguin_constants import FISH_SIZE, PENGUIN_SIZE
 
 class __Penguin:
     """
-    This class represents a Penguin avatar. It is passed in the args, x & y, which represents the x & y coordinates of the penguin.
-    It also passes in the args color, which represents the color of the penguin.
+    This class represents a Penguin avatar. 
     The color will be set to only the penguin colors defined in Constants, when a penguin instance is created.
-    This is a private class, as we don't want the user to have direct access to it. Functionality will be done
-    through board methods; this is why we don't error check x and y because that will be done in the board methods.
+    This is a private class, as I don't want the user to have direct access to it. Functionality (including error checking x and y) will be done
+    through board methods.
     :param x: the x coordinate of the penguin
     :param y: the y coordinate of the penguin
     :param color: the color of the penguin
